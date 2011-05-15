46ffbb7a88c4eba0339220fbb78d5ad75461453f
Igor Podolskiy <igor.podolskiy@vwi-stuttgart.de>
2011-05-15 15:06:23 +0200

    * add tests for fast-used-way

---
+++ src/test/de/vwistuttgart/openstreetmap/osmosis/fastusedfilter/v0_6/FastUsedWayFilterTest.java
@@ -0,0 +1,76 @@
+package de.vwistuttgart.openstreetmap.osmosis.fastusedfilter.v0_6;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.openstreetmap.osmosis.core.domain.v0_6.EntityType;
+import org.openstreetmap.osmosis.core.filter.common.IdTrackerType;
+import org.openstreetmap.osmosis.test.task.v0_6.SinkEntityInspector;
+import org.openstreetmap.osmosis.xml.common.CompressionMethod;
+import org.openstreetmap.osmosis.xml.v0_6.FastXmlReader;
+
+import de.vwistuttgart.openstreetmap.osmosis.fastusedfilter.test.AcceptAllFilter;
+import de.vwistuttgart.openstreetmap.osmosis.fastusedfilter.test.EmptySource;
+import de.vwistuttgart.openstreetmap.osmosis.fastusedfilter.test.TestUtil;
+
+
+public class FastUsedWayFilterTest {
+
+	@Test
+	public void standard() throws Exception {
+		File inFile = TestUtil.makeDataFile("/data/input/v0_6/inputRelationWay.xml");
+		FastXmlReader reader1 = new FastXmlReader(inFile, false, CompressionMethod.None);
+		FastXmlReader reader2 = new FastXmlReader(inFile, false, CompressionMethod.None);
+		try {
+			SinkEntityInspector result = TestUtil.runFastUsedFilter(new FastUsedWayFilter(IdTrackerType.IdList, 1),
+					reader1, reader2, new AcceptAllFilter());
+			TestUtil.assertCount(1 + 1 + 2 + 6, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(1, EntityType.Bound, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(1, EntityType.Relation, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(2, EntityType.Way, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(6, EntityType.Node, result.getProcessedEntities());
+		} finally {
+			inFile.delete();
+		}
+	}
+
+
+	@Test
+	public void allEmpty() throws Exception {
+		SinkEntityInspector result = TestUtil.runFastUsedFilter(new FastUsedWayFilter(IdTrackerType.IdList, 1),
+				new EmptySource(), new EmptySource(), new AcceptAllFilter());
+		TestUtil.assertCount(0, result.getProcessedEntities());
+	}
+
+
+	@Test
+	public void waysEmpty() throws Exception {
+		File inFile = TestUtil.makeDataFile("/data/input/v0_6/inputRelationWay.xml");
+		FastXmlReader reader1 = new FastXmlReader(inFile, false, CompressionMethod.None);
+		try {
+			SinkEntityInspector result = TestUtil.runFastUsedFilter(new FastUsedWayFilter(IdTrackerType.IdList, 1),
+					new EmptySource(), reader1, new AcceptAllFilter());
+			TestUtil.assertCount(1 + 1 + 0 + 6, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(1, EntityType.Bound, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(1, EntityType.Relation, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(0, EntityType.Way, result.getProcessedEntities());
+			TestUtil.assertEntitiesCountByType(6, EntityType.Node, result.getProcessedEntities());
+		} finally {
+			inFile.delete();
+		}
+	}
+	
+	@Test
+	public void relationsEmpty() throws Exception {
+		File inFile = TestUtil.makeDataFile("/data/input/v0_6/inputBound.xml");
+		FastXmlReader reader1 = new FastXmlReader(inFile, false, CompressionMethod.None);
+		try {
+			SinkEntityInspector result = TestUtil.runFastUsedFilter(
+					new FastUsedNodeFilter(IdTrackerType.IdList, 1), 
+					reader1, new EmptySource(), new AcceptAllFilter());
+			TestUtil.assertCount(0, result.getProcessedEntities());
+		} finally {
+			inFile.delete();
+		}
+	}
+}
